401d9f73b9fe526f8bb4092bc39c54ac3fe4a570
Siddharth Dayalwal <siddharth.developer.cse@gmail.com>
2019-10-01 01:34:06 +0530

    Create Output.c

---
+++ Objective/1/Output.c
@@ -0,0 +1,9 @@
+// Guess the Output 
+    #include<stdio.h>
+    #define CUBE(x) (x*x*x)
+    int main()
+    {
+        int a, b=4;
+        a = CUBE(b++);
+        printf("%d, %d\n", a, b);
+    }
