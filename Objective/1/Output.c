65a55c64615b28824d9d36780e041fdd49781e50
Siddharth Dayalwal <siddharthdayalwal2000@gmail.com>
2019-10-18 14:56:34 +0530

    Update Output.c

--- Objective/1/Output.c
+++ Objective/1/Output.c
@@ -12,7 +12,8 @@
     }
 
 // Answer is  :  120, 7
-
-
-
-//                         //
+// 
+//  when the variable is passed , it is b=4 at initail
+//  then on increment it becomes b=5 and then again b=6
+//  so we get a=4*5*6=120
+//  then we have b++ --> b=7
